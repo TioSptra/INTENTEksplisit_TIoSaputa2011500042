f1e6bed9a9cbf62d300904fe06fbdee91dbd138c
TioSptra <tiosptra@gmail.com>
2022-06-07 10:04:21 +0700

    tugas9

---
+++ app/src/main/java/com/example/intent_tiosaputra2011500042/TampilDataActivity.kt
@@ -0,0 +1,24 @@
+package com.example.intent_tiosaputra2011500042
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class TampilDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampil_data)
+
+        val tvData = findViewById<TextView>(R.id.tvData)
+
+        val nim = intent.getStringExtra("NIM")
+        val nama = intent.getStringExtra("NAMA")
+
+        tvData.text ="""
+            Data yang dimasukan:
+            NIM: $nim
+            Nama: $nama
+         """.trimIndent()
+
+    }
+}
